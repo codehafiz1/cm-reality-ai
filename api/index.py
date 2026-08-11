8c90f0c8eb51ad3c820643105d6991f03615b742
Hafiz Shehryar <syedshehryarali59@gmail.com>
2026-08-11 20:19:33 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -60,3 +60,5 @@ def chat():
         return jsonify({"reply": f"Groq API Error: {response.text}"}), 500
     except Exception as e:
         return jsonify({"reply": f"Other Error: {str(e)}"}), 500
+
+
